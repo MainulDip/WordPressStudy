b711f4f4c23c93c4b702860f204b32cbf864cd8f
mainuldip <mainuldip@gmail.com>
2024-11-19 02:57:13 -0500

    block-editor handbook experimenting block-settings on block.json

--- Apps-wp-env/plugins/copyright-block-dynamic/build/index.asset.php
+++ Apps-wp-env/plugins/copyright-block-dynamic/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '86fad3b2b9f298e1dce9');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'a5db0e25dc02fd8a82d4');
--- Apps-wp-env/plugins/copyright-block-dynamic/build/index.js
+++ Apps-wp-env/plugins/copyright-block-dynamic/build/index.js
@@ -178,7 +178,7 @@ module.exports = window["wp"]["i18n"];
   \************************/
 /***/ ((module) => {
 
-module.exports = /*#__PURE__*/JSON.parse('{"$schema":"https://schemas.wp.org/trunk/block.json","apiVersion":3,"name":"create-block/copyright-block-dynamic","version":"0.1.0","title":"CopyrightD","category":"widgets","icon":"palmtree","description":"Copyright icon following string","example":{},"supports":{"html":false},"textdomain":"copyright-block-dynamic","editorScript":"file:./index.js","editorStyle":"file:./index.css","style":"file:./style-index.css","render":"file:./render.php","viewScript":"file:./view.js"}');
+module.exports = /*#__PURE__*/JSON.parse('{"$schema":"https://schemas.wp.org/trunk/block.json","apiVersion":3,"name":"create-block/copyright-block-dynamic","version":"0.1.0","title":"CopyrightD","category":"widgets","icon":"palmtree","description":"Copyright icon following string","example":{},"supports":{"color":{"background":false,"text":true},"html":false,"typography":{"fontSize":true}},"textdomain":"copyright-block-dynamic","editorScript":"file:./index.js","editorStyle":"file:./index.css","style":"file:./style-index.css","render":"file:./render.php","viewScript":"file:./view.js"}');
 
 /***/ })
 
